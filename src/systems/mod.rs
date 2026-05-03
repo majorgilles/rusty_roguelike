7efa3d784609bfc9181cc72e712044978f8e55e9
Gilles Major <major.gilles@gmail.com>
2026-05-04 00:10:16 +0200

    feat: add system for player inputs and add its system

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -2,5 +2,7 @@ use crate::prelude::*;
 mod player_inputs;
 
 pub fn build_scheduler() -> Schedule {
-    Schedule::builder().build()
+    Schedule::builder()
+        .add_system(player_inputs::player_input_system())
+        .build()
 }
