145f7ed5146525df3a73ce52f62c584668826e20
Gilles Major <major.gilles@gmail.com>
2026-05-03 13:35:31 +0200

    feat: private functions to carve tunnels

--- src/map_builder.rs
+++ src/map_builder.rs
@@ -29,6 +29,7 @@ impl MapBuilder {
                 }
             }
             if !overlap {
+                // For each point of the room, convert it to a Floor tile
                 room.for_each(|p| {
                     if p.x > 0 && p.x < SCREEN_WIDTH && p.y > 0 && p.y < SCREEN_HEIGHT {
                         let idx = map_idx(p.x, p.y);
@@ -37,6 +38,24 @@ impl MapBuilder {
                 });
             }
             self.rooms.push(room)
+        }
+    }
+
+    fn apply_vertical_tunnel(&mut self, y1: i32, y2: i32, x: i32) {
+        use std::cmp::{max, min};
+        for y in min(y1, y2)..=max(y1, y2) {
+            if let Some(idx) = self.map.try_idx(Point::new(x, y)) {
+                self.map.tiles[idx] = TileType::Floor;
+            }
+        }
+    }
+
+    fn apply_horizontal_tunnel(&mut self, x1: i32, x2: i32, y: i32) {
+        use std::cmp::{max, min};
+        for x in min(x1, x2)..=max(x1, x2) {
+            if let Some(idx) = self.map.try_idx(Point::new(x, y)) {
+                self.map.tiles[idx] = TileType::Floor;
+            }
         }
     }
 }
