069049f9d1b2dbca3921620b483f31c4273822b7
Gilles Major <major.gilles@gmail.com>
2026-05-05 23:19:57 +0200

    refactor: rename variables for clarity in tooltip position calculation

--- src/systems/tooltip.rs
+++ src/systems/tooltip.rs
@@ -8,12 +8,12 @@ pub fn tooltips(ecs: &SubWorld, #[resource] mouse_pos: &Point, #[resource] camer
     let mut positions = <(Entity, &Point, &Name)>::query();
 
     let offset = Point::new(camera.left_x, camera.top_y);
-    let map_pos = *mouse_pos + offset;
+    let mouse_map_pos = *mouse_pos + offset;
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(2);
     positions
         .iter(ecs)
-        .filter(|(_, pos, _)| **pos == map_pos)
+        .filter(|(_, pos, _)| **pos == mouse_map_pos)
         .for_each(|(entity, _, name)| {
             let screen_pos = *mouse_pos * 2;
             let display =
